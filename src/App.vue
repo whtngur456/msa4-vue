d75038b8fd48576a760eaa963c8166fc2e78e37d
whtngur456 <p99344786@gmail.com>
2026-04-16 17:06:15 +0900

    20260416 vue limit~dataBinding

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// js 코드 작성 부분
+import DataBinding from './components/DataBinding.vue';
+</script>
+   
+<template>
+<!-- html 작성부분 -->
+  <DataBinding />
+</template>
+
+<style>
+/* css작성 부분 */
+</style>
---
+++ src/components/DataBinding.vue
@@ -0,0 +1,62 @@
+<script setup>
+// js 코드 작성 부분
+import { reactive, ref } from 'vue';
+
+// ref(0)
+// 타입제한 없이 사용할수 있으나, 프라이미티브 타입에 반응적 참조를 위해 사용
+// 프라이미티브 타입에 좀더 나은 성능을 제공
+const cnt = ref(0);
+// ref객체의 경우 js에서 접근시, 해당값은 ref,value프로퍼티로 접근
+console.log(cnt.value);
+
+function add() {
+  cnt.value++;
+}
+//reactive()
+//객체만 사용 가능하고, 해당 객체의 반응적 참조를 위해 사용
+const info = reactive({
+  name: 'hong',
+  age: 20
+});
+console.log(info.name);
+const color = ref('red');
+function btnToggle() {
+  if(color.value === 'red') {
+    color.value = 'blue';
+  } else {
+    color.value = 'red'; 
+  }
+}
+
+const id = ref('');
+</script>
+
+<template>
+  <!-- 하지만 template에서는 해당 객체면 만으로 값에 접근 가능 하다 -->
+<!-- html 작성부분 -->
+ <h1 :class="color">데이터 바인딩{{ cnt }}</h1>
+ <button type="button" @click="cnt++">증가</button>
+  <button type="button" @click="cnt--">감소</button>
+ <!-- HTML요소와 Vue 인스턴스의 데이터를 동저으로 연결하는 기능 -->
+  <h3>{{ `${info.name} : ${info.age}` }}</h3>
+  <!-- 태그 속성명 앞에 ':'을 붙이고, 속성값에 데이터 바인딩 객체를 사용 -->
+  <h3 class="test" :class="color">태그 속성값 데이터 바인딩</h3>
+  <button type="button" @click="btnToggle">색깔 토글</button>
+
+  <!-- 양방향 데이터 바인딩 -->
+   <input type="text" v-model="id">
+   <p>{{ id }}</p>
+</template>
+
+<style>
+.red {
+  color: red;
+}
+.blue {
+  color: blue;
+}
+.test {
+  color:aqua;
+}
+/* css작성 부분 */
+</style>
